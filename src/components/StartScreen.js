244a62bb15f853933a84ad412a08b1a5031671e1
Raul de Souza <raulsouza180802@gmail.com>
2022-08-21 02:27:27 -0300

    first commit

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div>
+      <h1>Secret Word</h1>
+      <p>Clique no botão abaixo para começar a jogar</p>
+      <button>Começar jogo</button>
+    </div>
+  );
+};
+
+export default StartScreen;
