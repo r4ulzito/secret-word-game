b8d42d32bf81cb9dd6e707b63084baf458a0e794
Raul de Souza <raulsouza180802@gmail.com>
2022-08-22 09:58:55 -0300

    update

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -2,7 +2,7 @@ import "./StartScreen.css";
 
 const StartScreen = () => {
   return (
-    <div>
+    <div className="start">
       <h1>Secret Word</h1>
       <p>Clique no botão abaixo para começar a jogar</p>
       <button>Começar jogo</button>
